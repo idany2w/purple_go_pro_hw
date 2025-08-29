8527c2873f3d71cf3902f65571965948db85cf79
d.ruzin <d.ruzin@pikabu.ru>
2025-08-29 21:58:16 +0300

    Add logging functionality to order API
    - Introduced logrus for structured logging in main.go.
    - Updated go.mod and go.sum to include logrus dependency.
    - Implemented a logger initialization function and integrated logging middleware for HTTP requests.

---
+++ 4-order-api/pkg/middleware/common.go
@@ -0,0 +1,13 @@
+package middleware
+
+import "net/http"
+
+type WrapperWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (w *WrapperWriter) WriteHeader(statusCode int) {
+	w.ResponseWriter.WriteHeader(statusCode)
+	w.statusCode = statusCode
+}
