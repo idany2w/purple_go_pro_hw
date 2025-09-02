101b7071315a8b78b87c5be8ff1d092a9d15afd5
d.ruzin <d.ruzin@pikabu.ru>
2025-09-03 00:04:22 +0300

    Remove unused API modules and files
    - Deleted go.mod and main.go files for 1-concurrency, 2-random-api, and 3-validation-api as they are no longer needed.
    - Removed associated handler files and configuration examples to clean up the project structure.

--- 3-validation-api/internal/verify/payload.go
+++
@@ -1,9 +0,0 @@
-package verify
-
-type VerifySendEmailRequest struct {
-	Email string `json:"email" validate:"required,email"`
-}
-
-type VerifyHashRequest struct {
-	IsVerified bool `json:"is_verified"`
-}
--- 3-validation-api/pkg/request/validate.go
+++
@@ -1,9 +0,0 @@
-package request
-
-import (
-	"github.com/go-playground/validator/v10"
-)
-
-func IsValid[T any](payload T) error {
-	return validator.New().Struct(payload)
-}
